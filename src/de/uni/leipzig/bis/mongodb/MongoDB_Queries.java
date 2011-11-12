e400b67ee289a36b14392e2bf70509c0d9c94f39
s1ck <martin.junghanns@gmx.net>
2011-11-12 18:48:27 +0100

    added treshold query and some docu

--- src/de/uni/leipzig/bis/mongodb/MongoDB_Queries.java
+++ src/de/uni/leipzig/bis/mongodb/MongoDB_Queries.java
@@ -8,62 +8,175 @@ import com.mongodb.DBCollection;
 
 public class MongoDB_Queries {
 
+	/**
+	 * Represents the datatypes of the stored values
+	 * 
+	 * @author s1ck
+	 * 
+	 */
+	public static enum DataType {
+		UDC, PDC, PAC, TEMP, GAIN
+	}
+
+	/**
+	 * Defines the number of warmup runs
+	 */
+	public static final int SKIPS = 10;
+
+	/**
+	 * Defines the number of test runs
+	 */
+	public static final int RUNS = 100;
+
+	/**
+	 * Processes a range query between two given timestamps.
+	 * 
+	 * @param mongoDB
+	 *            mongoDB instance
+	 * @param fromTimestamp
+	 *            the lower bound of the range
+	 * @param toTimestamp
+	 *            the upper bound of the range
+	 */
 	public static void rangeQuery1(DB mongoDB, long fromTimestamp,
 			long toTimestamp) {
 		// get collection
 		DBCollection measCollection = mongoDB
 				.getCollection(MongoDB_eval.COLLECTION_MEASUREMENT);
-						
+
 		System.out.println("Query: Range Query 1");
 		System.out.println("********************");
 		System.out.printf("from:\t%s\n", new Date(fromTimestamp));
-		System.out.printf("to:\t%s\n", new Date(toTimestamp));				 		
-		
-		// range query
-		BasicDBObject rangeQuery = new BasicDBObject();		
-		// greater than, lower than
-		rangeQuery.put("timestamp", new BasicDBObject("$gt", fromTimestamp).append("$lt", toTimestamp));
+		System.out.printf("to:\t%s\n", new Date(toTimestamp));
 
-		long start = System.currentTimeMillis();
-		measCollection.find(rangeQuery).count();
-		long diff = System.currentTimeMillis() - start;
+		// range query
+		BasicDBObject rangeQuery = new BasicDBObject();
+		// greater than, lower than
+		rangeQuery.put("timestamp", new BasicDBObject("$gt", fromTimestamp)
+				.append("$lt", toTimestamp));
+
+		long diff = processQuery(measCollection, rangeQuery);
 
 		System.out.printf("took [ms]:\t%d\n", diff);
-		System.out.println("********************");				
+		System.out.println("********************\n\n");
 	}
-	
-	public static void createIndex(DB mongoDB, String collectionName, String indexName) {
+
+	/**
+	 * Returns all timestamp where a given value was above a given treshold
+	 * 
+	 * @param mongoDB
+	 *            mongoDB instance
+	 * @param datatype
+	 *            see {@link DataType} for available DataTypes
+	 * @param treshold
+	 *            Treshold as integer value
+	 */
+	public static void tresholdQuery1(DB mongoDB, DataType datatype,
+			int treshold) {
 		// get collection
-		DBCollection collection = mongoDB
-				.getCollection(collectionName);
-		
+		DBCollection measCollection = mongoDB
+				.getCollection(MongoDB_eval.COLLECTION_MEASUREMENT);
+
+		System.out.println("Query: Treshold Query 1");
+		System.out.println("********************");
+		System.out.printf("datatype:\t%s\n", datatype);
+		System.out.printf("treshold:\t%d\n", treshold);
+
+		BasicDBObject tresholdQuery = new BasicDBObject();
+		tresholdQuery.put("datatype", datatype.toString());
+		tresholdQuery.put("value", new BasicDBObject("$gt", treshold));
+
+		long diff = processQuery(measCollection, tresholdQuery);
+
+		System.out.printf("took [ms]:\t%d\n", diff);
+		System.out.println("********************\n\n");
+	}
+
+	/**
+	 * Creates an index on the given attribute in the given collection.
+	 * 
+	 * @param mongoDB
+	 * @param collectionName
+	 * @param indexName
+	 */
+	public static void createIndex(DB mongoDB, String collectionName,
+			String indexName) {
+		// get collection
+		DBCollection collection = mongoDB.getCollection(collectionName);
+
 		System.out.println("Query: Create Index");
 		System.out.println("********************");
-		
+		System.out.printf("index name:\t%s\n", indexName);
+
 		long start = System.currentTimeMillis();
 		collection.ensureIndex(indexName);
 		long diff = System.currentTimeMillis() - start;
-		
+
 		System.out.printf("took [ms]:\t%d\n", diff);
-		System.out.println("********************");		
+		System.out.println("********************\n\n");
 	}
-	
-	public static void dropIndex(DB mongoDB, String collectionName, String indexName) {
+
+	/**
+	 * Drops an index from an attribute at a given collection
+	 * 
+	 * @param mongoDB
+	 * @param collectionName
+	 * @param indexName
+	 */
+	public static void dropIndex(DB mongoDB, String collectionName,
+			String indexName) {
 		// get collection
-		DBCollection collection = mongoDB
-				.getCollection(collectionName);
-		
+		DBCollection collection = mongoDB.getCollection(collectionName);
+
 		System.out.println("Query: Drop Index");
 		System.out.println("********************");
-		
+		System.out.printf("index name:\t%s\n", indexName);
+
 		long start = System.currentTimeMillis();
 		collection.dropIndex(indexName + "_1");
 		long diff = System.currentTimeMillis() - start;
-		
+
 		System.out.printf("took [ms]:\t%d\n", diff);
-		System.out.println("********************");	
+		System.out.println("********************\n\n");
 	}
-	
-	
+
+	/**
+	 * Processes a given query including warmup and test runs.
+	 * 
+	 * @param coll
+	 *            the collection to perform the query on
+	 * @param query
+	 *            the query
+	 * @param skips
+	 *            number of warmup runs
+	 * @param runs
+	 *            number of runs
+	 * @return
+	 */
+	private static long processQuery(DBCollection coll, BasicDBObject query) {
+		long[] times = new long[RUNS];
+		long start;
+		long diff;
+
+		// warmup
+		for (int i = 0; i < SKIPS; i++) {
+			coll.find(query).count();
+		}
+
+		// testing
+		for (int i = 0; i < RUNS; i++) {
+			start = System.currentTimeMillis();
+			coll.find(query).count();
+			diff = System.currentTimeMillis() - start;
+			times[i] = diff;
+		}
+
+		long sum = 0L;
+		for (int i = 0; i < times.length; i++) {
+			sum += times[i];
+		}
+
+		return sum / times.length;
+	}
 
 }
--- src/de/uni/leipzig/bis/mongodb/MongoDB_eval.java
+++ src/de/uni/leipzig/bis/mongodb/MongoDB_eval.java
@@ -15,19 +15,27 @@ import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoException;
 
+import de.uni.leipzig.bis.mongodb.MongoDB_Queries.DataType;
+
 public class MongoDB_eval {
 
-	public static final String 	HOST 					= "localhost";
-	public static final Integer PORT 					= 27017;
-	public static final String 	DB 						= "BIS_mongo_eval";
-	public static final String 	COLLECTION_MEASUREMENT 	= "measurings";	
-	public static final String	COLLECTION_STATIONS 	= "stations";
-	public static final Integer BUFFER_SIZE 			= 10000;
-	
-	public static final String PATH_380K				= "data/380K.csv";
-	public static final String PATH_6M					= "data/6mio.csv";
-	public static final String PATH_30M					= "data/30nio.csv";
-	
+	/**
+	 * Database specific settings
+	 */
+	public static final String HOST = "localhost";
+	public static final Integer PORT = 27017;
+	public static final String DB = "BIS_mongo_eval";
+	public static final String COLLECTION_MEASUREMENT = "measurings";
+	public static final String COLLECTION_STATIONS = "stations";
+	public static final Integer BUFFER_SIZE = 10000;
+
+	/**
+	 * Data paths
+	 */
+	public static final String PATH_380K = "data/380K.csv";
+	public static final String PATH_6M = "data/6mio.csv";
+	public static final String PATH_30M = "data/30nio.csv";
+
 	public static void main(String[] args) throws UnknownHostException,
 			MongoException {
 		// open connection
@@ -36,68 +44,111 @@ public class MongoDB_eval {
 		DB db = m.getDB(DB);
 
 		// create collections (if not existing)
-		if(db.getCollection(COLLECTION_MEASUREMENT) == null) {
+		if (db.getCollection(COLLECTION_MEASUREMENT) == null) {
 			db.createCollection(COLLECTION_MEASUREMENT, null);
 		}
-		if(db.getCollection(COLLECTION_STATIONS) == null) {
+		if (db.getCollection(COLLECTION_STATIONS) == null) {
 			db.createCollection("stations", null);
 		}
-		
-		importMeasuringPoints(PATH_380K, db);
-		testMeasurementData(db);
-		
+
+		// importMeasuringPoints(PATH_6M, db);
+		testMeasurementData(db, false);
+
 		// close connection
 		m.close();
 	}
-	
-	private static void testMeasurementData(DB mongoDB) {
+
+	/**
+	 * Performs a set of queries on the mongoDB instance.
+	 * 
+	 * @param mongoDB
+	 *            the mongoDB instance to perform queries on
+	 * @param useIndices
+	 *            true if indices shall be used for the queries (takes time to
+	 *            create / drop)
+	 */
+	private static void testMeasurementData(DB mongoDB, boolean useIndices) {
+		// create indices
+		if (useIndices) {
+			MongoDB_Queries.createIndex(mongoDB, COLLECTION_MEASUREMENT,
+					"timestamp");
+			MongoDB_Queries.createIndex(mongoDB, COLLECTION_MEASUREMENT,
+					"datatype");
+			MongoDB_Queries.createIndex(mongoDB, COLLECTION_MEASUREMENT,
+					"value");
+		}
+
+		// do some queries
 		MongoDB_Queries.rangeQuery1(mongoDB, 1314277800000L, 1314282900000L);
-		MongoDB_Queries.createIndex(mongoDB, COLLECTION_MEASUREMENT, "timestamp");
-		MongoDB_Queries.rangeQuery1(mongoDB, 1314277800000L, 1314282900000L);
-		MongoDB_Queries.dropIndex(mongoDB, COLLECTION_MEASUREMENT, "timestamp");		
+		MongoDB_Queries.tresholdQuery1(mongoDB, DataType.GAIN, 20000);
+
+		// drop indices
+		if (useIndices) {
+			MongoDB_Queries.dropIndex(mongoDB, COLLECTION_MEASUREMENT,
+					"timestamp");
+			MongoDB_Queries.dropIndex(mongoDB, COLLECTION_MEASUREMENT,
+					"datatype");
+			MongoDB_Queries.dropIndex(mongoDB, COLLECTION_MEASUREMENT, "value");
+		}
 	}
-	
+
+	/**
+	 * Loads measuring data from a given CSV file into a mongoDB instance.
+	 * 
+	 * @param csvFile
+	 *            Path (absolute or relative to the csv file)
+	 * @param mongoDB
+	 *            the mongoDB instance
+	 */
+	@SuppressWarnings("unused")
 	private static void importMeasuringPoints(String csvFile, DB mongoDB) {
-		List<BasicDBObject> dbObjectPuffer = new ArrayList<BasicDBObject>(BUFFER_SIZE);
-		BufferedReader bf;		
+		List<BasicDBObject> dbObjectPuffer = new ArrayList<BasicDBObject>(
+				BUFFER_SIZE);
+		BufferedReader bf;
 		try {
 			bf = new BufferedReader(new FileReader(csvFile));
-			
+
 			String line;
 			long n = 0L;
-			
+
 			System.out.println("importing data");
 			long start = System.currentTimeMillis();
-			
-			while((line = bf.readLine()) != null) {
+
+			while ((line = bf.readLine()) != null) {
 				n++;
 				dbObjectPuffer.add(createDBObjectFromData(line));
-				if(dbObjectPuffer.size() == BUFFER_SIZE) {
+				if (dbObjectPuffer.size() == BUFFER_SIZE) {
 					// store data and clear buffer
 					writeDataToDB(dbObjectPuffer, mongoDB);
 					dbObjectPuffer.clear();
 					System.out.print(".");
-					if(n / BUFFER_SIZE % 10 == 0)
+					if (n / BUFFER_SIZE % 10 == 0)
 						System.out.printf(" %d documents inserted\n", n);
-					
+
 				}
 			}
 			long diff = System.currentTimeMillis() - start;
-			System.out.printf("took %d seconds for %d documents (%d documents / s)", diff / 1000, n, n / (diff / 1000));
+			System.out.printf(
+					"took %d seconds for %d documents (%d documents / s)",
+					diff / 1000, n, n / (diff / 1000));
 			bf.close();
-		} catch(FileNotFoundException fEx) {
-			
-		} catch(IOException ioEx)
-		{
-			
+		} catch (FileNotFoundException fEx) {
+			System.err.println(fEx);
+		} catch (IOException ioEx) {
+			System.err.println(ioEx);
 		}
-		
-		
 	}
-	
+
+	/**
+	 * Creates a Database Object from a given csv String
+	 * 
+	 * @param line
+	 *            CSV data information about the database object
+	 * @return the Database Object
+	 */
 	private static BasicDBObject createDBObjectFromData(String line) {
 		String[] documentData = line.split(";");
-		
+
 		BasicDBObject dbObj = new BasicDBObject();
 		// timestamp
 		dbObj.put("timestamp", Long.parseLong(documentData[0]));
@@ -105,8 +156,7 @@ public class MongoDB_eval {
 		dbObj.put("value", Integer.parseInt(documentData[1]));
 		// identifier
 		String identifier = documentData[2];
-		if(identifier != null)
-		{
+		if (identifier != null) {
 			String[] identifierData = identifier.split("\\.");
 			// station_ID (Anlagenname)
 			dbObj.put("stationID", identifierData[0]);
@@ -117,21 +167,31 @@ public class MongoDB_eval {
 			// datatype (Datenart)
 			dbObj.put("datatype", identifierData[3]);
 			// optional data
-			if(identifierData[3].equals("pdc") || identifierData[3].equals("udc")) {
+			if (identifierData[3].equals("pdc")
+					|| identifierData[3].equals("udc")) {
 				// "string"
 				dbObj.put("opt1", identifierData[4]);
 				// serial number 2
 				dbObj.put("opt2", identifierData[5]);
 			}
-		}				
+		}
 		return dbObj;
 	}
-	
+
+	/**
+	 * writes a set of documents to the database
+	 * 
+	 * @param documents
+	 *            a set of database objects (documents)
+	 * @param mongoDB
+	 *            mongoDB instance where the documents are stored
+	 */
 	private static void writeDataToDB(List<BasicDBObject> documents, DB mongoDB) {
-		DBCollection measurementCollection =  mongoDB.getCollectionFromString(COLLECTION_MEASUREMENT);
-		
-		for(DBObject dbObj : documents) { 
+		DBCollection measurementCollection = mongoDB
+				.getCollectionFromString(COLLECTION_MEASUREMENT);
+
+		for (DBObject dbObj : documents) {
 			measurementCollection.save(dbObj);
-		}				
+		}
 	}
 }
